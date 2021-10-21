13b05bb81b30dcb524d35af58fb9d4aa0ec727f5
Jose Fco <jsuague220a@ieslacampina.es>
2021-10-21 13:54:08 +0200

    dos

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,10 +1,19 @@
 UPDATE usuario1
-SET apellido = 'RODRIGUEZ'
-WHERE nombre = 'ANTONIO';
+SET apellido = 'Manuel'
+WHERE nombre = 'pedro';
 
 UPDATE aula2
 SET numero = 3
 WHERE numero = 1;
 
+UPDATE curso3
+SET nombre = 'IAW'
+WHERE nombre = 'Fundamentos de Hardware';
 
+UPDATE edificio4
+SET numero = 4
+WHERE numero = 1;
 
+UPDATE edificio4
+SET numero = 4
+WHERE numero = 1;
