1044c29c6b8e251d93292472c789aee23db340b6
Jose Fco <jsuague220a@ieslacampina.es>
2021-10-21 13:34:13 +0200

    ERROR9

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,4 +1,6 @@
-
+UPDATE USUARIO1 
+SET 
+WHERE ;
 
 UPDATE AULA2
 SET 
@@ -11,9 +13,3 @@ WHERE ;
 UPDATE AULA2
 SET 
 WHERE ;
-UPDATE personas
-SET apellido2 = 'RODRIGUEZ'
-WHERE nombre = 'ANTONIO'
-AND apellido1 = 'GARCIA'
-AND apellido2 = 'BENITO'
-AND APELLIDO3= 'GG'
