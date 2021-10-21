55375a202792fd9444cd1a761b7e328c080d9176
Jose Fco <jsuague220a@ieslacampina.es>
2021-10-21 13:50:45 +0200

    arreglado2

---
+++ UPDATE.sql
@@ -0,0 +1,15 @@
+UPDATE usuario1
+SET apellido = 'RUIZ'
+WHERE nombre = 'NICOLAS';
+
+UPDATE aula2
+SET numero = 5
+WHERE numero = 2;
+
+UPDATE curso3
+SET nombre = 'SAD'
+WHERE nombre = 'Sistema operativo';
+
+UPDATE edificio4
+SET numero = 4
+WHERE numero = 5;
